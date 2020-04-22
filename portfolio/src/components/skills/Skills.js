349fbf4379eed55d2f2748f9948790677ed32a93
JC Smiley <jcsmileyjr@gmail.com>
2020-04-22 07:40:00 -0500

    Setup file  and page structure of Portfolio Page
    - Create initial Skills and ProjectSection components

---
+++ portfolio/src/components/skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container, Row, Col, Image} from 'react-bootstrap';
+
+const Skills = () => {
+	return(
+		<Container fluid>
+			<h1>Hello</h1>
+		</Container>
+	);
+}
+
+export default Skills;
