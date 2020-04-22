f90f96f3841855f90f69d69dbac6352f06e640b2
JC Smiley <jcsmileyjr@gmail.com>
2020-04-21 19:51:59 -0500

    Add files to show the Nav bar and setup SASS files
    - Create primary SASS file to hold global CSS  variables
    - Initial nav setup

---
+++ portfolio/src/components/nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import './nav.scss';
+
+const Nav = () => {
+	return(
+		<Container fluid className="nav-container">
+			<Row >
+				<Col><p>hello world</p></Col>
+			</Row>
+			
+		</Container>
+	);
+}
+
+export default Nav;
---
+++ portfolio/src/pages/portfolio/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import Nav from '../../components/nav/Nav.js';
+
+const Portfolio = () => {
+	return(
+		<Container>
+			<Nav />
+			<h1>Hello World</h1>
+		</Container>
+	);
+}
+
+export default Portfolio;
