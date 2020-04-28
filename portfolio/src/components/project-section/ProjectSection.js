c6c9c3826d3ab657208a049ce71b4e1f8ff046a0
Smiley <jcsmileyjr@gmail.com>
2020-04-28 15:42:12 -0500

    Convert Project component into a separate component from ProjectSectioin

--- portfolio/src/components/project-section/ProjectSection.js
+++ portfolio/src/components/project-section/ProjectSection.js
@@ -2,40 +2,7 @@ import React from 'react';
 import {Container, Row, Col, Image, Button} from 'react-bootstrap';
 
 import './projectSection.scss';
-import HeadShot from '../../assets/personal/meetup-head-shot-small.jpg';
-import coderBoy from '../../assets/personal/africanmanpc.png';
-
-const Project = (props) => {
-	return(
-		<Col className="project-container">			
-			<Image src={HeadShot} className="img-fluid" responsive />			
-			<p>Title</p>
-			<p>Description</p>
-			<div className="project-display-tech">
-				<section className="tech-image-style">
-					<Image src={coderBoy} fluid />
-					<p>Tech</p>
-				</section>
-				<section className="tech-image-style">
-					<Image src={coderBoy} fluid />
-					<p>Tech</p>
-				</section>
-				<section className="tech-image-style">
-					<Image src={coderBoy} fluid />
-					<p>Tech</p>
-				</section>
-				<section className="tech-image-style">
-					<Image src={coderBoy} fluid />
-					<p>Tech</p>
-				</section>
-			</div>
-			<div className="project-links-container">
-				<a href="">GitHub</a>
-				<a href="">Demo</a>
-			</div>
-		</Col>
-	);
-}
+import Project from '../project/Project.js';
 
 const ProjectSection = () => {
 	return(
