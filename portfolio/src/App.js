b4343e46467fbcc5c322ad2e5ba8834be19b2a0d
Smiley <jcsmileyjr@gmail.com>
2020-06-06 12:04:41 -0500

    Added react router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './pages/portfolio/portfolio.scss';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import Portfolio from './pages/portfolio/Portfolio.js';
 
 function App() {
   return (
-    <div>
-      <Portfolio />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <Portfolio />
+          </Route>
+          <Route path="/about">
+            
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
