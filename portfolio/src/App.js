bfb0cd112ee67101bece672098990d3c9e43c4a4
Smiley <jcsmileyjr@gmail.com>
2020-06-22 10:58:54 -0500

    Refactored Header out of the pages into a permanent nav role

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './pages/portfolio/portfolio.scss';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
+import Header from './components/nav/Header';
 import Portfolio from './pages/portfolio/Portfolio.js';
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
           <Route path="/">
             <Portfolio />
