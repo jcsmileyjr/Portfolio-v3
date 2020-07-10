e07973be17a3d1e1569638959746473d45fc46d5
Smiley <jcsmileyjr@gmail.com>
2020-07-09 21:24:56 -0500

    Added more content from Medium articles

--- portfolio/src/data/blogPosts.js
+++ portfolio/src/data/blogPosts.js
@@ -12,6 +12,26 @@ const post = [
         content:"I have been trying to get into tech for a long time and have made every classic mistake possible. I hope by reading this, you can avoid these painful mistakes and get into the tech industry faster.",
         link:"https://medium.com/codeconnective/learning-new-skills-while-breaking-into-tech-b8793f61a1c2?source=friends_link&sk=df5c9187686a2c0dcc0e59bb9773e925",
     },
+    {
+        title:"What is a RESTful API",
+        content:"RESTful API is a way for a client (website, mobile app, etc.) to communicate to a web service regarding a resource (data, picture, web page, etc.). An example of this is when you use Google search. You will type in a parameter instructing Google to retrieve and display data for you to view. This article will explain what RESTful API is and how it is commonly used. You can read a brief guide to creating an API with Node and Express in the article A Simple Guide to Node/Express.",
+        link:"https://medium.com/codeconnective/what-is-a-restful-api-930671cdd713?source=friends_link&sk=3019c9cd263b72ee75964df33e2b4ebd",
+    },
+    {
+        title:"8 Tips to Improve Comments in Your Code",
+        content:"I have a problem; I know code should have comments but I don’t know how to write good comments or alternatives to commenting. I was given a take home project for a job interview. As a hot shot aspiring developer I went crazy writing code. The app looked beautiful, the code worked, and I documented every line of code.",
+        link:"https://medium.com/codeconnective/8-tips-to-improve-comments-in-your-code-59d2b638ee38?source=friends_link&sk=aff4fe9739c50fe911a0282b7dd1d268",
+    },
+    {
+        title:"How to Setup Multiple API Routes with Node & Express",
+        content:"In this article, we will: Create an API route that deploys an 'index.html', develop a router that has multiple API routes, and write an API that returns a json object.",
+        link:"https://medium.com/codeconnective/how-to-setup-multiple-api-routes-with-node-express-12b7cabcd13c?source=friends_link&sk=1defc009388bf6ea2e7d04ef382649db",
+    },
+    {
+        title:"A Simple Guide to Node/Express",
+        content:"This is a stupidly simple short guide full of pictures to create and start a Node web service with an Express API. This is the first article in a series using Node, Express, MongoDB, and React to teach the process of creating a simple full stack application.",
+        link:"https://medium.com/codeconnective/a-simple-guide-to-node-express-1986a9904d63?source=friends_link&sk=3cf75bf7adf93ddb04be07db07be199f",
+    },
 ];
 
 export default post;
