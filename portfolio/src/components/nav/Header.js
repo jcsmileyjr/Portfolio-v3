a7be5fcf21ce08e4288348e3dffdf2801edae108
Smiley <jcsmileyjr@gmail.com>
2021-02-05 05:29:52 -0600

    Add the E-book to the nav bar

--- portfolio/src/components/nav/Header.js
+++ portfolio/src/components/nav/Header.js
@@ -47,6 +47,9 @@ const Header = () => {
               </Link>
             </Nav.Item>
             <Nav.Item className="nav-links-container-style">
+                <a href="https://gumroad.com/l/breakingIntoTech" target="_blank" rel="noopener noreferrer" className="link-style">E-Book</a>
+            </Nav.Item>
+            <Nav.Item className="nav-links-container-style">
               <a
                 href={resume}
                 download
